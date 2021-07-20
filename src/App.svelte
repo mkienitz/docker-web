1055b14dbe26eb24de1c29263aedec627f584c65
Maximilian Kienitz <max@kienitz.dev>
2021-07-20 23:46:25 +0200

    Add Fragestunde BBB link

--- src/App.svelte
+++ src/App.svelte
@@ -59,6 +59,7 @@
 <main>
 	<h1 on:click={handleClick} style="--color : {color} ">{name}</h1>
 	<ul>
+		<li><a href="https://bbb.in.tum.de/max-4fn-vjd">Fragestunde - Mi 16:00</a></li>
 		<li><a href="https://terminplaner4.dfn.de/NAyTDhcGxD7Cqabt">Terminumfrage - Fragestunde</a></li>
 		{#each files as file}
 			<li><a href="/files/{file.filename}">{file.title}</a></li>
